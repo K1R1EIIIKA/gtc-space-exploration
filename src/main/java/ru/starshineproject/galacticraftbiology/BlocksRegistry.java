a6eb315f4ab1ff3cc05ce76ded8f52954502068a
K1R1EIIIKA <69672954+K1R1EIIIKA@users.noreply.github.com>
2021-02-13 20:42:51 +0400

    mod base

--- src/main/java/ru/starshineproject/galacticraftbiology/BlocksRegistry.java
+++ src/main/java/ru/starshineproject/galacticraftbiology/BlocksRegistry.java
@@ -1,6 +1,9 @@
 package ru.starshineproject.galacticraftbiology;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.relauncher.Side;
@@ -31,7 +34,7 @@ public class BlocksRegistry {
     @SideOnly(Side.CLIENT)
     private static void setRender(Block block)
     {
-
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
     }
 
 }
--- src/main/java/ru/starshineproject/galacticraftbiology/proxy/ClientProxy.java
+++ src/main/java/ru/starshineproject/galacticraftbiology/proxy/ClientProxy.java
@@ -3,6 +3,7 @@ package ru.starshineproject.galacticraftbiology.proxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import ru.starshineproject.galacticraftbiology.BlocksRegistry;
 
 public class ClientProxy extends CommonProxy {
 
@@ -15,6 +16,7 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void init(FMLInitializationEvent event)
     {
+        BlocksRegistry.registerRender();
         super.init(event);
     }
 
