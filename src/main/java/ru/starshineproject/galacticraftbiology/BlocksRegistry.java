d1eeb83b87dc7f4372c438033c77ae7097f9f525
S1dech <slaaaadd@gmail.com>
2021-02-14 17:14:39 +0700

    Переделал чутка регистрацию блоков, добавил табу, ордикт

--- src/main/java/ru/starshineproject/galacticraftbiology/BlocksRegistry.java
+++ src/main/java/ru/starshineproject/galacticraftbiology/BlocksRegistry.java
@@ -5,6 +5,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
@@ -20,26 +21,27 @@ import ru.starshineproject.galacticraftbiology.item.PlateSolar;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Objects;
 
 public class BlocksRegistry {
 
     public static void initBlock(){
         BlocksGB.moon_hive = new BlockMoonHive();
 
-        GalacticraftBiology.BLOCKS.forEach((block)->{
-            setRegister(block);
-        });
+        GalacticraftBiology.BLOCKS.forEach(BlocksRegistry::setRegister);
     }
 
     private static void setRegister(Block block) {
         ForgeRegistries.BLOCKS.register(block);
-        ForgeRegistries.ITEMS.register(new ItemBlock(block).setRegistryName(block.getRegistryName()));
+        ResourceLocation reg = Objects.requireNonNull(block.getRegistryName());
+        ForgeRegistries.ITEMS.register(new ItemBlock(block).setRegistryName(reg));
     }
 
     @SideOnly(Side.CLIENT)
     public static void setRender() {
         GalacticraftBiology.BLOCKS.forEach((block)->{
-            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
+            ResourceLocation reg = Objects.requireNonNull(block.getRegistryName());
+            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(reg, "inventory"));
         });
     }
 }
